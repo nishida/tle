fb217e591e77d172b3fd4361035374740b3c236f
Keisuke Nishida <6652402+nishida@users.noreply.github.com>
2020-11-02 20:34:43 +0900

    EPOCHの日付ごとにダウンロード

---
+++ download_json_day.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import spacetrack.operators as op
+from spacetrack import SpaceTrackClient
+from dateutil import parser
+from datetime import datetime, timedelta
+import time
+import sys
+import subprocess
+from subprocess import PIPE
+import spacetrackaccount
+
+def showusage():
+    print('Usage: {} YYYY-MM-DD [YYYY-MM-DD]'.format(sys.argv[0]))
+
+
+
+if len(sys.argv) == 3 :
+    start = parser.parse(sys.argv[1], yearfirst=True)
+    end = parser.parse(sys.argv[2], yearfirst=True)
+elif len(sys.argv) == 2 :
+    start = parser.parse(sys.argv[1], yearfirst=True)
+    end = start
+else :
+    print('Invalid number of arguments!')
+    showusage()
+    sys.exit(0)
+
+
+start = datetime(start.year, start.month, start.day)
+end = datetime(end.year, end.month, end.day)
+
+if end < start:
+	start, end = end, start
+
+ndays = (end - start).days + 1
+
+print('Start:', start.strftime('%Y-%m-%d'))
+print('End:', end.strftime('%Y-%m-%d'))
+print('Days:', ndays)
+
+st = SpaceTrackClient(spacetrackaccount.userid, spacetrackaccount.password)
+
+def mycallback(until):
+    duration = int(round(until - time.monotonic()))
+    print('Sleeping for {:d} seconds.'.format(duration))
+
+st.callback = mycallback
+
+for day in (start + timedelta(days=i) for i in range(ndays)):
+    print('Downloading', day.strftime('%Y-%m-%d'))
+
+    nextday = day + timedelta(days=1)
+
+    data = st.gp_history(epoch=[op.inclusive_range(day.strftime('%Y-%m-%d'), nextday.strftime('%Y-%m-%d'))], 
+        orderby=['norad_cat_id', 'epoch'], format='json')
+    file = 'download/{}.json'.format(day.strftime('%Y%m%d'))
+    with open(file, 'w') as fp:
+        fp.write(data)
+    proc = subprocess.Popen(['xz', '-9', file], stdout=PIPE, stderr=PIPE, text=True)
+
+    # Limit API queries to less than 30 requests per minute / 300 requests per hour
+    if day != (start + timedelta(days = (ndays - 1))):
+        time.sleep(12)
+
+    (stdout, stderr) = proc.communicate()
+
+    if stdout != '' or stderr != '' :
+        print(stdout, stderr)
+        sys.exit(0)
+
+sys.exit(1)
+
